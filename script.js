697bb10869d769af8a2422105b2d598054507eb0
pokemaughan <kayleemaughan@gmail.com>
2017-10-05 20:53:04 +0000

    debugging response in js file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var buttonSelected = null;
+var buttonSelected = 'australian'; //default. null is a bad value to set it to
 
 var setButton = function(selected){
 	buttonSelected = selected;
@@ -7,7 +7,7 @@ var setButton = function(selected){
 	console.log("button: "+ buttonSelected)
 }
 
-$(document).ready(function(){
+$(document).ready(function(){ // im pretty sure there's a way to pass a parameter from the html to the js function so we can differentiate what button is selected right now
 	$("#inputField").keyup(function(e){
 		console.log("input: " + $("#inputField").val());
 		e.preventDefault();
@@ -17,6 +17,13 @@ $(document).ready(function(){
 			dataType: "json",
 			success: function(json){
 				console.log("data: " + json);
+
+				$.each(json,function(key, value){
+					console.log("key " +key+ " value "+value)
+					$.each(value, function(key,value){
+						console.log("key " +key+ " value "+value)
+					})
+				})
 				//var everything = contents[translation]; //not exactly sure that gets it
 				var everything ="";
 				$("translatedField").html(everything);
