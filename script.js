d7d6419df2b1e6b65235ab99065c7460900d9cd9
pokemaughan <kayleemaughan@gmail.com>
2017-10-05 20:03:52 +0000

    sript changes

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-$(document).ready(function(){ // im pretty sure there's a way to pass a parameter from the html to the js function so we can differentiate what button is selected right now
-	$("#inputField").keyup(function(){
-		var url = "http://api.funtranslations.com/translate/?" //+ whatever button is clicked
-			+$("#inputField").val();
-		$.getJSON(url,function(data){
-			var everything = contents[translation]; //not exactly sure that gets it
-			$("translatedField").html(everything);
+
+var buttonSelected = null;
+
+var setButton = function(selected){
+	buttonSelected = selected;
+	//console.log("selected: "+ selected)
+	console.log("button: "+ buttonSelected)
+}
+
+$(document).ready(function(){
+	$("#inputField").keyup(function(e){
+		console.log("input: " + $("#inputField").val());
+		e.preventDefault();
+		var myurl = "http://api.funtranslations.com/translate/" + buttonSelected + ".json?text=" +$("#inputField").val();
+		$.ajax({
+			url: myurl,
+			dataType: "json",
+			success: function(json){
+				console.log("data: " + json);
+				//var everything = contents[translation]; //not exactly sure that gets it
+				var everything ="";
+				$("translatedField").html(everything);
+			}
 		})
 	})
 })
+
